10f74f076faeb410d64b5da8224b220a41a04f56
gethub-crypto <fernadester761@gmail.com>
2026-03-18 21:11:26 +0800

    Update levels.js

--- game/levels.js
+++ game/levels.js
@@ -1,19 +1,21 @@
 function createLevels() {
 
     const map = document.getElementById("map");
+    map.innerHTML = "";
 
     for (let i = 1; i <= 50; i++) {
 
         let btn = document.createElement("button");
-
         btn.className = "levelBtn";
 
         if (i % 10 === 0) btn.classList.add("super");
 
-        btn.innerText = i;
+        let stars = playerProgress[i]?.stars || 0;
+
+        btn.innerHTML = `${i}<br>${"⭐".repeat(stars)}`;
 
         btn.onclick = () => startLevel(i);
 
         map.appendChild(btn);
     }
-}
+        }
